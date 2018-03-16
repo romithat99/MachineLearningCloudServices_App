67c659f55541cb555d63df76642758cf90a21323
romitha <romitha@enadoc.com>
2018-03-16 10:19:10 +0530

    first commit

--- app/src/main/java/com/example/romitha/machinelearningcloudservicesapp/SecondActivity.java
+++ app/src/main/java/com/example/romitha/machinelearningcloudservicesapp/SecondActivity.java
@@ -1,15 +1,35 @@
 package com.example.romitha.machinelearningcloudservicesapp;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SecondActivity();
+            }
+        });
+    }
+    public void SecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
 
     }
+
 }
